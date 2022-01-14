6b996c15236f6a14bb549d4aae2c6b1c597b9c55
ganudoomer <ganuiscool@gmail.com>
2022-01-15 01:23:58 +0530

    Initial Commit

---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Sree on 15/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
